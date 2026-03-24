fe2c8e5c3f2406c0f5b5c57235fc64a5caf91989
SaltinStillWaters <SaltinStillWaters@gmail.com>
2026-03-24 14:38:49 +0800

    refactor: improve ui and logic

--- src/views/User/Inventory.vue
+++ src/views/User/Inventory.vue
@@ -1,83 +1,160 @@
 <template>
-  <v-data-table-server
-    v-model:items-per-page="itemsPerPage"
-    :headers="headers"
-    :items="serverItems"
-    :items-length="totalItems"
-    :loading="loading"
-    :search="search"
-    item-value="name"
-    @update:options="fetchInventory"
-  >
-    <template v-slot:tfoot>
-      <!-- <tr>
-        <td>
-          <v-text-field v-model="name" class="ma-2" density="compact" placeholder="Search name..." hide-details></v-text-field>
-        </td>
-        <td>
+  <v-card elevation="2" class="rounded-lg border">
+    <v-card-title class="d-flex align-center px-4 py-3 bg-grey-lighten-4">
+      <v-icon
+        icon="mdi-package-variant-closed"
+        color="amber-darken-2"
+        class="me-2"
+      />
+      <span class="text-subtitle-1 font-weight-bold">Inventory</span>
+      <v-spacer />
+      <v-btn
+        color="amber-darken-2"
+        variant="flat"
+        prepend-icon="mdi-plus"
+        size="small"
+      >
+        Add Product
+      </v-btn>
+    </v-card-title>
+
+    <v-divider />
+
+    <v-card-text class="bg-grey-lighten-5 pt-4">
+      <v-row dense align="center"> 
+        
+        <v-col cols="12" md="3">
           <v-text-field
-            v-model="calories"
-            class="ma-2"
+            v-model="searchEAN"
+            label="EAN / Barcode"
+            prepend-inner-icon="mdi-barcode-scan"
+            variant="outlined"
             density="compact"
-            placeholder="Minimum calories"
-            type="number"
+            bg-color="white"
+            color="amber-darken-2"
+            clearable
             hide-details
-          ></v-text-field>
-        </td>
-      </tr> -->
-    </template>
-  </v-data-table-server>
+            :disabled="!!searchStock"
+            @keyup.enter="fetchInventory"
+          />
+        </v-col>
+        <v-col cols="12" md="3">
+          <v-text-field
+            v-model="searchName"
+            label="Product Name"
+            prepend-inner-icon="mdi-magnify"
+            variant="outlined"
+            density="compact"
+            bg-color="white"
+            color="amber-darken-2"
+            clearable
+            hide-details
+            :disabled="!!searchStock"
+            @keyup.enter="fetchInventory"
+          />
+        </v-col>
+
+        <v-col cols="12" md="1" class="text-center">
+          <span class="text-caption font-weight-bold text-grey-darken-1">OR</span>
+        </v-col>
+
+        <v-col cols="12" md="3">
+          <v-text-field
+            v-model="searchStock"
+            label="Max Stock Level"
+            prepend-inner-icon="mdi-package-down"
+            variant="outlined"
+            density="compact"
+            bg-color="white"
+            color="amber-darken-2"
+            type="number"
+            clearable
+            hide-details
+            :disabled="!!searchEAN || !!searchName"
+            @keyup.enter="fetchInventory"
+          />
+        </v-col>
+
+        <v-col cols="12" md="2">
+          <v-btn
+            color="amber-darken-2"
+            variant="flat"
+            prepend-icon="mdi-magnify"
+            block
+            @click="fetchInventory"
+          >
+            Search
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-card-text>
+
+    <v-divider />
+
+    <v-data-table-server
+      v-model:items-per-page="limit"
+      :headers="headers"
+      :items="serverItems"
+      :items-length="totalItems"
+      :loading="loading"
+      item-value="ean"
+      hover
+      @update:options="fetchInventory"
+    >
+      <template v-slot:loading>
+        <v-skeleton-loader type="table-row@5" />
+      </template>
+    </v-data-table-server>
+  </v-card>
 </template>
 
 <script setup>
-  import api from '@/axios'
-  import { ref, watch } from 'vue'
+import api from "@/axios";
+import { ref } from "vue";
 
-  const loading = ref(true)
-  const itemsPerPage = ref(5)
-  const totalItems = ref(0)
-  const searchName = ref('')
-  const searchEAN = ref('')
+const loading = ref(true);
+const limit = ref(5);
+const totalItems = ref(0);
+const searchName = ref("");
+const searchEAN = ref("");
+const searchStock = ref();
 
-  const headers = ref([
-    {
-      title: 'EAN',
-      align: 'start',
-      sortable: false,
-      key: 'EAN',
-    },
-    { title: 'Name', key: 'name', align: 'start' },
-    { title: 'Stock', key: 'stock', align: 'end' },
-  ])
+const headers = ref([
+  {
+    title: "EAN",
+    align: "start",
+    sortable: false,
+    key: "EAN",
+  },
+  { title: "Name", key: "name", align: "start", sortable: false },
+  { title: "Stock", key: "stock", align: "start", sortable: false },
+]);
 
-  const serverItems = ref([])
+const serverItems = ref([]);
 
-  const name = ref('')
-  const calories = ref('')
-  const search = ref('')
-  async function fetchInventory ({ page, itemsPerPage }) {
-    loading.value = true
-    const result = await api.get(`/inventories?page=${page}&limit=${itemsPerPage}&name=${searchName.value}&EAN=${searchEAN.value}`);
-    console.log(result.data.data)
-    serverItems.value = result.data.data.data.map(inventory => {
-      return {
-        id: inventory.product._id,
-        EAN: inventory.product.EAN,
-        name: inventory.product.name,
-        stock: inventory.stock,
-      }
-    });
-    totalItems.value = result.data.data.pages * itemsPerPage
-    console.log(serverItems.value)
-    loading.value = false
+async function fetchInventory({ page=1, itemsPerPage }) {
+  loading.value = true;
+  const result = await api.get(`/inventories`, {
+    params: {
+      page, 
+      limit: itemsPerPage ?? limit.value,
+      name: searchName.value,
+      EAN: searchEAN.value,
+      maxStock: searchStock.value
+    }
+  });
 
-  }
+  serverItems.value = result.data.data.data.map((inventory) => {
+    return {
+      id: inventory.product._id,
+      EAN: inventory.product.EAN,
+      name: inventory.product.name,
+      stock: inventory.stock,
+    };
+  });
 
-  watch(name, () => {
-    search.value = String(Date.now())
-  })
-  
-  watch(calories, () => {
-    search.value = String(Date.now())
-  })
+  totalItems.value = result.data.data.totalItems;
+  console.log(totalItems.value)
+  loading.value = false;
+}
 </script>
